8cb3f1bf0e8217bafa1096a5f81b901fa59839b1
nicolae.spalatelu <nicolae.spalatelu@gohenry.co.uk>
2020-01-28 19:29:43 +0200

    add .gitignore file to skip files created by Intellij

--- homeworks/src/main/java/junit1/Calculator.java
+++ homeworks/src/main/java/junit1/Calculator.java
@@ -9,19 +9,101 @@ import java.util.regex.*;
 
 public class Calculator {
 
-    private String expression = "10 cm + 1 m - 10 mm ";
-    public final static String[] formats = {"mm", "cm", "dm", "m","km"};
-    private String outputType;
+    public final static String[] formats = {"mm", "cm", "dm", "m", "km"};
+    private String expression = "10 cm + 1 m - 10 mm + 10 mm";
     private List<String[]> list = new ArrayList<>();
+    private String outputType;
 
     public Calculator(String expression, String outputType) {
         this.expression = expression;
         this.outputType = outputType;
     }
 
-    public int conversionToMm(int value, String format){
+    public static String[] getFormats() {
+        return formats;
+    }
+
+    public int conversionToUpperMeasure(int result, String outputType) {
+        switch (outputType) {
+            case "cm":
+                result *= 10;
+                break;
+
+            case "dm":
+                result *= 100;
+                break;
+
+            case "m":
+                result *= 1000;
+                break;
+
+            case "km":
+                result *= 1000000;
+                break;
+
+        }
+
+        return result;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+
+    public String getOutputType() {
+        return outputType;
+    }
+
+    public void setOutputType(String outputType) {
+        this.outputType = outputType;
+    }
+
+    public int split(String expression, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(expression);
+        int result = 0;
+        while (matcher.find()) {
+            list.add(matcher.group(0).split(" "));
+        }
+
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String[] item = (String[]) iterator.next();
+            int value = conversionToMm(Integer.parseInt(item[0]), item[1]);
+            item[0] = String.valueOf(value);
+        }
+
+        int size = 0;
+        if (list.size() == 1) {
+            result = Integer.parseInt(list.get(0)[0]);
+        } else if (list.size() == 2) {
+            if (list.get(size)[2].equals("+")) {
+                result += add(Integer.parseInt(list.get(size)[0]), Integer.parseInt(list.get(size + 1)[0]));
+            } else {
+                result += substract(Integer.parseInt(list.get(size)[0]), Integer.parseInt(list.get(size + 1)[0]));
+            }
+        } else {
+
+            result = add(Integer.parseInt(list.get(size)[0]), Integer.parseInt(list.get(size + 1)[0]));
+            while (++size < list.size() - 1) {
+                if (list.get(size)[2].equals("+")) {
+                    result = add(result, Integer.parseInt(list.get(size + 1)[0]));
+                } else {
+                    result = substract(result, Integer.parseInt(list.get(size + 1)[0]));
+                }
+            }
+
+        }
+        return result;
+    }
+
+    public int conversionToMm(int value, String format) {
         int converted;
-        switch (format){
+        switch (format) {
             case "cm":
                 converted = value * 10;
                 break;
@@ -45,102 +127,14 @@ public class Calculator {
         return converted;
     }
 
-    public int add(int a, int b){
+    public int add(int a, int b) {
 
         return a + b;
     }
 
-    public int substract(int a, int b){
+    public int substract(int a, int b) {
 
-        return a-b;
-    }
-
-    public int conversionToUpperMeasure(int result, String outputType){
-        switch (outputType){
-            case "cm":
-                result *= 10;
-                break;
-
-            case "dm":
-                result *= 100;
-                break;
-
-            case "m":
-                result *= 1000;
-                break;
-
-            case "km":
-                result *= 1000000;
-                break;
-
-        }
-
-        return result;
-    }
-
-    public int split(String expression, String regex){
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(expression);
-        int result = 0;
-        while(matcher.find()){
-            list.add(matcher.group(0).split(" "));
-        }
-
-        Iterator iterator = list.iterator();
-        while(iterator.hasNext()){
-            String[] item = (String[]) iterator.next();
-            int value = conversionToMm(Integer.parseInt(item[0]),item[1]);
-            item[0] = String.valueOf(value);
-        }
-
-        int size = 0;
-        if(list.size()==1){
-            result = Integer.parseInt(list.get(0)[0]);
-        }
-
-        else if(list.size()==2){
-            if(list.get(size)[2].equals("+")){
-                result += add(Integer.parseInt(list.get(size)[0]), Integer.parseInt(list.get(size+1)[0]));
-            }
-            else{
-                result += substract(Integer.parseInt(list.get(size)[0]), Integer.parseInt(list.get(size+1)[0]));
-            }
-        }
-
-        else{
-
-            result = add(Integer.parseInt(list.get(size)[0]), Integer.parseInt(list.get(size+1)[0]));
-            while(++size < list.size()-1){
-                if(list.get(size)[2].equals("+")){
-                    result = add(result, Integer.parseInt(list.get(size+1)[0]));
-                }
-                else{
-                    result = substract(result, Integer.parseInt(list.get(size+1)[0]));
-                }
-            }
-
-        }
-        return result;
-    }
-
-    public String getExpression() {
-        return expression;
-    }
-
-    public void setExpression(String expression) {
-        this.expression = expression;
-    }
-
-    public static String[] getFormats() {
-        return formats;
-    }
-
-    public String getOutputType() {
-        return outputType;
-    }
-
-    public void setOutputType(String outputType) {
-        this.outputType = outputType;
+        return a - b;
     }
 }
 
